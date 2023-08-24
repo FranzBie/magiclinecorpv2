4543ed4215b3d9dba2b69348bb2322aa447aa888
3d <oikawasenpai01@gmail.com>
2023-08-24 09:16:05 +0800

    small changes on blade
    updated label for add product to inform user max size for uploads

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -209,6 +209,7 @@
         </div>
     </div>
 
+    <!-- Scripts -->
     {{-- datables --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
